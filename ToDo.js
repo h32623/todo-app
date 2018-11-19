11200459e60b1c334eab1af6db561e55e2f8ffdd
h32623 <gofhd9633@naver.com>
2018-11-19 17:21:19 +0900

    state 따라 style 변경되는 부분 수정

--- ToDo.js
+++ ToDo.js
@@ -6,19 +6,30 @@ const {width, height} = Dimensions.get("window");
 export default class ToDo extends Component{
 
   state = {
-    isEditing: false
-  }
+    isEditing: false,
+    isCompleted: false
+  };
 
   render(){
+    const {isCompleted} = this.state;
     return (
-      <View style={styles.container}>    
-        <TouchableOpacity>
-          <View style={styles.circle} />
+      <View style={styles.container}>
+        <TouchableOpacity onPress={this._toggleComplete}>
+          <View style={[styles.circle, isCompleted ? styles.completedCircle : styles.uncompletedCircle]} />
         </TouchableOpacity>
-        <Text style={styles.text}>Hello I am a To Do</Text>
+        <Text style={[styles.text, isCompleted? styles.completedText : styles.uncompletedText]}>Hello I am a To Do</Text>
       </View>
     );
   }
+  _toggleComplete = () => {
+    this.setState(prevState => {
+      return {
+        isCompleted: !prevState.isCompleted
+      };
+    });
+  }
+
+
 }
 
 const styles = StyleSheet.create({
@@ -33,12 +44,27 @@ const styles = StyleSheet.create({
     width: 30,
     height: 30,
     borderRadius: 15,
-    backgroundColor: "red",
+    borderColor: "red",
+    borderWidth: 3,
+    marginLeft: 10,
     marginRight: 20
+  },
+  uncompletedCircle: {
+    borderColor: "#F23657"
+  },
+  completedCircle: {
+    borderColor: "#bbb"
   },
   text: {
     fontWeight: "600",
     fontSize: 20,
     marginVertical: 20
+  },
+  completedText: {
+    color: "#bbb",
+    textDecorationLine: "line-through"
+  },
+  uncompletedText: {
+    color: "#353839"
   }
 });
